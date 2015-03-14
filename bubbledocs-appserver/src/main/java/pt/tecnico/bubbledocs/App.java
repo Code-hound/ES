e7e35eb3eb73729861772ac309529dab8fd4ee1e
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-03-14 11:25:26 +0000

    ERROR
    [ATTENTION] Update with compile error

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/App.java
@@ -0,0 +1,13 @@
+package pt.tecnico.bubbledocs;
+
+/**
+ * Hello world! So para testar isto...
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello BubbleDocs World!" );
+    }
+}
