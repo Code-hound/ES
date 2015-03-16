73f9ccb79e31af947a7c339e4ba8d9e93bec7380
JPZef <jp.zef@hotmail.com>
2015-03-16 19:05:50 +0000

    Update User.java
    ja nao e possivel haver duas folhas do mesmo user com o mesmo nome

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/User.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/User.java
@@ -1,6 +1,9 @@
+
 package pt.tecnico.bubbledocs.domain;
 
 import org.jdom2.Element;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User extends User_Base {
     
@@ -10,7 +13,55 @@ public class User extends User_Base {
         set_name(name);
         set_password(password);
     }
-   
+    
+    private SpreadSheet getDocumentById(int id){
+    	for(SpreadSheet document : getDocsSet()){
+    		if(document.get_id().equals(id)){
+    			return document;
+    		}
+    		
+    	}
+    	return null;
+    }
+    
+    public boolean hasDocument(int documentId){
+    	return getDocumentById(documentId) != null;
+    }	 
+ 
+    public void addDocument(User userName, SpreadSheet spreadSheetToBeAdded) { //will throw exception if it already exists
+    	
+    	
+    	if(hasDocument(spreadSheetToBeAdded.get_id()))
+    		throw new IdAlreadyExistsException(spreadSheetToBeAdded.getId());
+    	
+    
+    	/*if (userName.viewSheets() != 0) //supondo que caso nao haja sheets do utilizador responde 0
+    	{
+    		if (super.viewSheetfromUser(userName, spreadSheetToBeAdded) != 0)
+    			super.addDocs(spreadSheetToBeAdded);
+    	}
+    	*/
+    	// nao foi possivel adicionar sheet, existe uma com nome igual
+    }
+    
+    public void removeDocument(int spreadSheetId) { //will throw exception if it already exists
+    	/*SpreadSheet toRemove = getDocumentById(spreadSheetId);
+    	*
+    	*if(toRemove == null)
+    	*  throw new SpreadSheetDoesNotExist(spreadSheetId);
+    	*/
+    	removeDocument(spreadSheetId);
+    }
+    
+    public List<SpreadSheet> getDocumentsByName(String spreadSheetName){
+    	List<SpreadSheet> docList = new ArrayList<SpreadSheet>();
+    	for(SpreadSheet s :getDocsSet()){
+    		if(s.get_spreadSheetName().equalsIgnoreCase(spreadSheetName))
+    			docList.add(s);
+    	}
+    	return docList;
+    }
+    
     public User createUser(String userName,String name, String password){
     	//if(!get_username().equals("root"))
     		//throws InvalidAccessException
@@ -29,4 +80,5 @@ public class User extends User_Base {
     	
     	return element;
     }
+    
 }
