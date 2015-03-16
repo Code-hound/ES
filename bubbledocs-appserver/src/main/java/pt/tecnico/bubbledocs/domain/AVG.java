fe2831a1fb2471a891a99449e61e1ddb44040681
alinecaliente <alinecaliente@hotmail.de>
2015-03-16 22:25:23 +0000

    Functions>importFromXML

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/AVG.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/AVG.java
@@ -31,7 +31,14 @@ public class AVG extends AVG_Base {
 
 	@Override
 	public void importFromXML(Element contentElement) {
-		// TODO Auto-generated method stub
+		Element arg1 = contentElement.getChild("arg1");
+		Element arg2 = contentElement.getChild("arg2");
+    	Reference r1 = new Reference();
+    	Reference r2 = new Reference();
+    	r1.importFromXML(arg1);
+    	r2.importFromXML(arg2);
+    	setReference(r1);
+    	setReference(r2);
 		
 	}
 }
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/SUB.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/SUB.java
@@ -40,7 +40,14 @@ public class SUB extends SUB_Base {
 
 	@Override
 	public void importFromXML(Element contentElement) {
-		// TODO Auto-generated method stub
+		Element arg1 = contentElement.getChild("arg1");
+		Element arg2 = contentElement.getChild("arg2");
+    	Content c1 = new Content();
+    	Content c2 = new Content();
+    	c1.importFromXML(arg1);
+    	c2.importFromXML(arg2);
+    	setContent(c1);
+    	setContent(c2);
 		
 	}
 }
