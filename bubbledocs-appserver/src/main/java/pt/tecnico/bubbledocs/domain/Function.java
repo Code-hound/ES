e916453018e53faa4aaed4aa1beda143fa019a0d
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-03-17 01:09:38 +0000

    Update Function.java

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Function.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Function.java
@@ -13,4 +13,18 @@ public abstract class Function extends Function_Base {
     	addArgs(arg2);
     }
     
+    @Override
+	public void importFromXML(Element contentElement) {
+    	/*
+		Element arg1 = contentElement.getChild("arg1");
+		Element arg2 = contentElement.getChild("arg2");
+    	Content c1 = new Content();
+    	Content c2 = new Content();
+    	c1.importFromXML(arg1);
+    	c2.importFromXML(arg2);
+    	setContent(c1);
+    	setContent(c2);
+    	*/
+		
+	}
 }
