94fdbf3b506c0c17bb40218eaff4fbc2786e7ef4
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-03-17 01:10:01 +0000

    Update ADD.java

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/ADD.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/ADD.java
@@ -29,18 +29,4 @@ public class ADD extends ADD_Base {
     	}
     	return element;
     }
-
-	@Override
-	public void importFromXML(Element contentElement) {
-		/*
-		Element arg1 = contentElement.getChild("arg1");
-		Element arg2 = contentElement.getChild("arg2");
-    	Content c1 = new Content();
-    	Content c2 = new Content();
-    	c1.importFromXML(arg1);
-    	c2.importFromXML(arg2);
-    	setContent(c1);
-    	setContent(c2);
-		*/
-	}
 }
