6dd6617871deca5eda47c1e01004ee200e0eb9aa
FMCalisto <francisco_mcalisto@hotmail.com>
2015-03-19 18:40:37 +0000

    New Commit Error

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/BubbleDocsException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/BubbleDocsException.java
@@ -2,6 +2,14 @@ package pt.tecnico.bubbledocs.exception;
 
 public class BubbleDocsException extends RuntimeException {
 	
+	/*
+	 *	BUBBLEDOCS EXCEPTION
+	 *
+	 *	Ficheiro "copiado" do exemplo phonebook
+	 *
+	 *	@author: Francisco Maria Calisto
+	 */
+	
 	private static final long serialVersionUID = 1L;
 
 }
