dc2f358ff2e4ec9c83d47d0d8f951ecca39e5778
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-03-22 21:16:25 +0000

    Update
    Update file with the new CreateUser.java file source.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -6,14 +6,24 @@ import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.UserDoesNotExistException;
 
 public class CreateUser extends BubbleDocsService {
+	
+	private String userToken;
+	private String newUsername;
+	private String password;
+	private String name;
 
     public CreateUser(String userToken, String newUsername,
             String password, String name) {
-	// add code here
+    	// add code here
+    	this.userToken = userToken;
+    	this.newUsername = newUserName;
+    	this.password = password;
+    	this.name = name;
     }
 
     @Override
     protected void dispatch() throws BubbleDocsException {
-	// add code here
+    	// add code here
+    	getBubbleDocs().addUser(new User(newUserName, name, password));
     }
 }
