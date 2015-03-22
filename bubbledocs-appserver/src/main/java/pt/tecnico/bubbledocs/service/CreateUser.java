3ff68b53c642540748f427153bde0c26c3c8c352
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-03-22 20:48:33 +0000

    Start again
    New files with the basic new code.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -1,39 +1,12 @@
-/*
 package pt.tecnico.bubbledocs.service;
-package pt.tecnico.bubbledocs.exception;
-*/
 
 // add needed import declarations
 
-/*
-
-
-import pt.tecnico.bubbledocs.domain.User;
-import pt.tecnico.bubbledocs.domain.BubbleDocs;
-
-import pt.tecnico.bubbledocs.exception.UserDoesNotExistException;
-
-import pt.tecnico.bubbledocs.service.BubbleDocsService;
-
-*/
-
-/*
-
 public class CreateUser extends BubbleDocsService {
-	private String userToken = "root";
-	
-    public CreateUser(String userToken, String newUsername, String password, String name) {
-    	BubbleDocs bubbledocs = BubbleDocs.getInstance();
-    	
-    	User user = new User(userToken);
-    	bubbledocs.addUser(user); //TODO
-    	try {
-    		user = new User("root"); //TODO
-    		//user.setUser(userToken); //TODO
-    		System.out.printls("Error! \n User violation.");
-    	} catch (pt.tecnico.phonebook.exception.UserDoesNotExistsException udne) {
-    		System.out.println("Could not create a root user");
-    	}
+
+    public CreateUser(String userToken, String newUsername,
+            String password, String name) {
+	// add code here
     }
 
     @Override
@@ -41,5 +14,3 @@ public class CreateUser extends BubbleDocsService {
 	// add code here
     }
 }
-
-*/
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
@@ -2,20 +2,15 @@ package pt.tecnico.bubbledocs.service;
 
 // add needed import declarations
 
-/*
 public class DeleteUser extends BubbleDocsService {
 
     public DeleteUser(String userToken, String toDeleteUsername) {
 	// add code here
     }
 
-    
     @Override
     protected void dispatch() throws BubbleDocsException {
 	// add code here
     }
-    
 
 }
-
-*/
