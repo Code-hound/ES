b60504035d9bf1e08df84a24af91e24877b6dd97
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-03-24 21:36:46 +0000

    Fixed
    Fixed Create and Delete User.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
@@ -3,34 +3,33 @@ package pt.tecnico.bubbledocs.service;
 //the needed import declarations
 
 import pt.tecnico.bubbledocs.domain.User;
+
 import pt.tecnico.bubbledocs.exception.UserDoesNotExistException;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 
 public class DeleteUser extends BubbleDocsService {
-	
+
 	private String userToken;
 	private String toDeleteUsername;
 
-    public DeleteUser(String userToken, String toDeleteUsername) {
-    	// add code here
-    	this.userToken = userToken;
-    	this.toDeleteUsername = toDeleteUsername;
-    }
+	public DeleteUser(String userToken, String toDeleteUsername) {
+		// add code here
+		this.userToken = userToken;
+		this.toDeleteUsername = toDeleteUsername;
+	}
 
-    @Override
-    protected void dispatch() throws BubbleDocsException {
-    	// add code here
-    	//User u = get_username(userToken); 			<--TODO
-    	//User tdun = u.get_username(toDeleteUsername); <--TODO
-    	
-    	/*
-    	if(tdun == null)
-    	{
-    		throw new UserDoesNotExistException(this.toDeleteUsername);
-    	}
-    	*/
-    	
-    	//tdun.delete(); TODO
-    }
+	@Override
+	protected void dispatch() throws UserDoesNotExistException,
+			BubbleDocsException {
+		if (userToken == "root") {
+			User userToDelete = getUser(toDeleteUsername);
+
+			if (userToDelete != null) {
+				getUser(userToken).removeUser(userToDelete);
+			} else {
+				throw new UserDoesNotExistException(this.toDeleteUsername);
+			}
+		}
+	}
 
 }
