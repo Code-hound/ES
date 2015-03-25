9c6acabcc5a672faba9ac3b6a702bd3a89a90356
JPZef <jp.zef@hotmail.com>
2015-03-25 02:38:07 +0000

    literal to cell feito, sem permissoes e sem token

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignLiteralCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignLiteralCell.java
@@ -1,18 +1,41 @@
 package pt.tecnico.bubbledocs.service;
 
+//redefinir para ficar apenas os importes necessários necessários!!!!!
+import pt.tecnico.bubbledocs.*;
+//split e preciso importar?????
+
 // add needed import declarations
+
+
 
 public class AssignLiteralCell extends BubbleDocsService {
     private String result;
 
     public AssignLiteralCell(String accessUsername, int docId, String cellId,
             String literal) {
-	// add code here	
+    	
+    	this.accessUsername = acceddUsername;
+    	this.docId = docId; 
+    	this.cellId = cellId;
+    	this.literal = literal;
+    	
+    	
+    	
     }
 
     @Override
     protected void dispatch() throws BubbleDocsException {
 	// add code here
+    	
+    	String[] cellRow = cellId.split(";");
+    	String row = cellRow[0]; 
+    	String collumn = cellRow[1];
+    	
+    	row = parse.int(cellId);
+    	//columns = 
+    	getBubbleDocs().getSpreadSheetById(docId).addContent(literal, row, column);
+    	result = literal;
+    	
     }
 
     public String getResult() {
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignReferenceCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignReferenceCell.java
@@ -1,5 +1,7 @@
 package pt.tecnico.bubbledocs.service;
 
+//redefinir para ficar apenas os importes necessários necessários
+import pt.tecnico.bubbledocs.*;
 // add needed import declarations
 
 public class AssignReferenceCell extends BubbleDocsService {
@@ -7,6 +9,12 @@ public class AssignReferenceCell extends BubbleDocsService {
 
     public AssignReferenceCell(String tokenUser, int docId, String cellId,
             String reference) {
+    	
+    	this.tokenUser = tokenUser;
+    	this.docId = docId;
+    	this.cellId = cellId;
+    	this.reference = reference;
+    	
 	// add code here
     }
 
