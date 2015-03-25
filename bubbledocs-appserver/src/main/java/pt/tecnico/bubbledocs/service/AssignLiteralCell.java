091ad3edf45c83f1e776cbb1112bfb7b075899db
JPZef <jp.zef@hotmail.com>
2015-03-25 03:13:31 +0000

    erros nos servicos assingliteraltocell e assign referencetocell, nao percebi onde, possivelmente impor, exceptions e/ou definir os this. ...

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignLiteralCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignLiteralCell.java
@@ -1,45 +1,45 @@
-package pt.tecnico.bubbledocs.service;
+//FALTA: 
+//-->tokens e permissoes
+//-->tirar o * no import, para ficar menos abrangente
+//-->confirmar se e' precisso importar a funcao split
 
-//redefinir para ficar apenas os importes necessários necessários!!!!!
-import pt.tecnico.bubbledocs.*;
-//split e preciso importar?????
-
-// add needed import declarations
-
-
-
-public class AssignLiteralCell extends BubbleDocsService {
-    private String result;
-
-    public AssignLiteralCell(String accessUsername, int docId, String cellId,
-            String literal) {
-    	
-    	this.accessUsername = acceddUsername;
-    	this.docId = docId; 
-    	this.cellId = cellId;
-    	this.literal = literal;
-    	
-    	
-    	
-    }
-
-    @Override
-    protected void dispatch() throws BubbleDocsException {
-	// add code here
-    	
-    	String[] cellRow = cellId.split(";");
-    	String row = cellRow[0]; 
-    	String collumn = cellRow[1];
-    	
-    	row = parse.int(cellId);
-    	//columns = 
-    	getBubbleDocs().getSpreadSheetById(docId).addContent(literal, row, column);
-    	result = literal;
-    	
-    }
-
-    public String getResult() {
-        return result;
-    }
-
-}
+//
+//package pt.tecnico.bubbledocs.service;
+//
+////redefinir para ficar apenas os importes necessarios necessarios!!!!!
+//import pt.tecnico.bubbledocs.*;
+////split e preciso importar?????
+//
+//// add needed import declarations
+//
+//public class AssignLiteralCell extends BubbleDocsService {
+//    private String result;
+//
+//    public AssignLiteralCell(String accessUsername, int docId, String cellId,
+//            String literal) {
+//    	
+//    	this.accessUsername = acceddUsername;
+//    	this.docId = docId; 
+//    	this.cellId = cellId;
+//    	this.literal = literal;
+//    
+//    }
+//
+//    @Override
+//    protected void dispatch(){ //throws BubbleDocsException {
+//	// add code here
+//    	
+//    	String[] rowAndColumn = cellId.split(";");
+//    	String row = rowAndColumn[0]; 
+//    	String collumn = rowAndColumn[1];
+//    	
+//    	getBubbleDocs().getSpreadSheetById(docId).addContent(literal, row, column);
+//    	result = literal;
+//    	
+//    }
+//
+//    public String getResult() {
+//        return result;
+//    }
+//
+//}
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignReferenceCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignReferenceCell.java
@@ -1,29 +1,64 @@
-package pt.tecnico.bubbledocs.service;
+//FALTA: 
+//-->tokens e permissoes
+//-->tirar o * no import, para ficar menos abrangente
+//-->confirmar se e' precisso importar a funcao split
 
-//redefinir para ficar apenas os importes necessários necessários
-import pt.tecnico.bubbledocs.*;
-// add needed import declarations
-
-public class AssignReferenceCell extends BubbleDocsService {
-    private String result;
-
-    public AssignReferenceCell(String tokenUser, int docId, String cellId,
-            String reference) {
-    	
-    	this.tokenUser = tokenUser;
-    	this.docId = docId;
-    	this.cellId = cellId;
-    	this.reference = reference;
-    	
-	// add code here
-    }
-
-    @Override
-    protected void dispatch() throws BubbleDocsException {
-	// add code here
-    }
-
-    public final String getResult() {
-        return result;
-    }
-}
+//package pt.tecnico.bubbledocs.service;
+//
+////redefinir para ficar apenas os importes necessarios necessarios
+//import pt.tecnico.bubbledocs.*;
+//// add needed import declarations
+//
+//public class AssignReferenceCell extends BubbleDocsService {
+//    private String result;
+//
+//    public AssignReferenceCell(String tokenUser, int docId, String cellId,
+//            String reference) {
+//    	
+//    /*	this.tokenUser = tokenUser;
+//    	this.docId = docId;
+//    	this.cellId = cellId;
+//    	this.reference = reference;
+//    */	
+//	// add code here
+//    }
+//
+//    @Override
+//    protected void dispatch(){ //throws BubbleDocsException {
+//	// add code here
+//    	
+//    	String[] rowAndColumnCell = cellId.split(";");
+//    	String rowCell = rowAndColumnCell[0]; 
+//    	String collumnCell = rowAndColumnCell[1];
+//    	
+//    	String[] rowAndColumnContent = reference.split(";");
+//    	String rowContent = rowAndColumnContent[0]; 
+//    	String collumnContent = rowAndColumnContent[1];
+//    	
+//    	int rowSpreadSheet = getBubbleDocs().getSpreadSheetById(docId).get_numberColumns();
+//    	int collumnSpreadSheet = getBubbleDocs().getSpreadSheetById(docId).get_numberRows();
+//    	
+//    	//se entrar nos dois if a reference aponta 
+//    	//para uma cell existente nas dimensoes da spreadsheet
+//    	if ((rowContent >= 0) && (rowContent <= rowSpreadSheet))
+//    	{
+//    		if ((columnContent >= 0) && (columnContent <= columnSpreadSheet))
+//    		{
+//    			getBubbleDocs().getSpreadSheetById(docId).addContent(reference, row, column);
+//    		}
+//    		else 
+//    		{
+//    			//throw new BubbleDocsException();
+//    		}
+//    	}
+//    	else 
+//    	{
+//    		//throw new BubbleDocsException();
+//    	}
+//    	
+//    }
+//
+//    public final String getResult() {
+//        return result;
+//    }
+//}
