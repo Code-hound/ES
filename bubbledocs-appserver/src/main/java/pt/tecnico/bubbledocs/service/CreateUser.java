9a2f3d491e9acdde501412b5b5c0aac98ef4961c
Francisco Maria Calisto <francisco.calisto@tecnico.ulisboa.pt>
2015-03-27 22:12:33 +0000

    Hard Changes
    Adding new Tests files and changing sorce code for Create and Delete
    User.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -3,7 +3,11 @@ package pt.tecnico.bubbledocs.service;
 // the needed import declarations
 
 import pt.tecnico.bubbledocs.domain.User;
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+
 import pt.tecnico.bubbledocs.exception.UserDoesNotExistException;
+import pt.tecnico.bubbledocs.exception.UserIsNotRootException;
+import pt.tecnico.bubbledocs.exception.UserAlreadyExistException;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 
 /*
@@ -16,23 +20,45 @@ import pt.tecnico.bubbledocs.exception.BubbleDocsException;
  * 
  */
 
-public class CreateUser extends BubbleDocsService {
+public class CreateUser extends BubbleDocsService
+{
 	
 	private String userToken;
 	private String newUsername;
 	private String password;
 	private String name;
 
-    public CreateUser(String userToken, String newUsername,
-            String password, String name) {
-    	this.userToken = userToken;
-    	this.newUsername = newUsername;
+    public CreateUser(String userToken, String newUsername, String password, String name)
+    {
+		this.userToken = userToken;
+		this.newUsername = newUsername;
     	this.password = password;
     	this.name = name;
     }
 
     @Override
-    protected void dispatch() throws BubbleDocsException {
-    	getBubbleDocs().addUser(new User(newUsername, name, password));
+    protected void dispatch() throws UserIsNotRootException, UserAlreadyExistException, UserDoesNotExistException, BubbleDocsException
+    {
+    	
+    	if(userToken == "root")
+    	{
+    		
+    		User userToVerify = getUser(newUsername);
+    		
+    		if(userToVerify != getUser(userToken))
+    		{
+    			getBubbleDocs().addUser(new User(newUsername, name, password));
+    		}
+    		else
+    		{
+    			throw new UserAlreadyExistException(this.newUsername);
+    		}
+    	}
+    	else
+    	{
+    		throw new UserIsNotRootException(this.newUsername);
+    	}
+    	
     }
+    
 }
