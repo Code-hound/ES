928827d3ff6ed8c1433e49c9471a9addcb779984
lribeirogomes <luis.ribeiro.gomes@tecnico.ulisboa.pt>
2015-03-31 02:52:53 +0100

    added protection exception

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/ProtectedCellException.java
@@ -0,0 +1,39 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class ProtectedCellException extends BubbleDocsException
+{
+
+	/*
+	 *	PROTECTED CELL EXCEPTION EXCEPTION
+	 *
+	 *	Excepcao caso a célula esteja protegida.
+	 *
+	 *	@author: Luís Ribeiro Gomes
+	 */
+
+	private int row;    //row
+	private int column; //column
+
+	public ProtectedCellException(int row, int column)
+	{
+		this.row    = row;
+		this.column = column;
+	}
+	
+	public int getRow()
+	{
+		return this.row;
+	}
+	
+	public int getColumn()
+	{
+		return this.column;
+	}
+	
+	@Override
+	public String getMessage()
+	{
+		return "Cell is protected: " + this.row + " ; " + this.column;
+	}
+	
+}
