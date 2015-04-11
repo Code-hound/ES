3a0263df9ea0f36b30bd3cf55760758e8e9e5834
FMCalisto <francisco_mcalisto@hotmail.com>
2015-04-11 19:14:42 +0100

    Create Unavailable Service Exception: fix #28

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/UnavailableServiceException.java
@@ -0,0 +1,30 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class UnavailableServiceException extends BubbleDocsException
+{
+
+	/*
+	 * UNAVAILABLE SERVICE EXCEPTION
+	 * 
+	 * Caso ocorra um erro na invocacao ao servico externo.
+	 * 
+	 * NOTA: alterar o servico de criacao de um user por forma a utilizar o servico
+	 * externo SD-ID via IDRemoteServices.
+	 * 
+	 * @author: Francisco Maria Calisto
+	 */
+
+	private static final long serialVersionUID = 1L;
+
+	public UnavailableServiceException()
+	{
+		super();
+	}
+
+	@Override
+	public String getMessage()
+	{
+		return "Unavailable service, try again.";
+	}
+
+}
