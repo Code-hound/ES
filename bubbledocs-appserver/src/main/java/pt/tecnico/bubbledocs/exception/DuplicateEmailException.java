25b28de1fa8cc9399d0403f4ff9117cc420fb040
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-04-12 21:26:56 +0100

    Create New Exceptions
    Create the new E-Mail, SD-ID and Remote Invocation Exceptions.

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/DuplicateEmailException.java
@@ -0,0 +1,30 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class DuplicateEmailException extends BubbleDocsException {
+
+	/*
+	 * DUPLICATE EMAIL EXCEPTION
+	 * 
+	 * Excepcao da varificacao se email ja existe.
+	 * 
+	 * @author: Francisco Maria Calisto
+	 */
+
+	private static final long serialVersionUID = 1L;
+
+	private String userEmail;
+
+	public DuplicateEmailException(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserEmail() {
+		return this.userEmail;
+	}
+
+	@Override
+	public String getMessage() {
+		return "This E-Mail " + this.userEmail + " is already being used";
+	}
+
+}
