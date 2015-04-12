7b52b13fc1d21ec2055a383415a79f59115cbc90
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-04-12 22:09:43 +0100

    Close Exception close #33
    Update the Invalid Username Exception.

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/InvalidUsernameException.java
@@ -0,0 +1,31 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class InvalidUserException extends BubbleDocsException {
+
+	/*
+	 * INVALID USERNAME EXCEPTION
+	 * 
+	 * Excepcao da varificacao se user e valido.
+	 * 
+	 * @author: Francisco Maria Calisto
+	 * 
+	 */
+
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+
+	public InvalidUserException(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	@Override
+	public String getMessage() {
+		return "This username " + this.userName + " is not valid";
+	}
+
+}
