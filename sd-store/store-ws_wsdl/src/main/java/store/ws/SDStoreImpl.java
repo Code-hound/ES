95b9bf40c424a98f76a990a2acb0ceb861407a62
Francisco Silveira <franciscosilveira463@gmail.com>
2015-04-12 17:31:57 +0100

    Added a new test DeleteDocuments, now with what so far has proven to be a fully-functional creation and deletion methods for pre- and post-test handling, respectively. There is some sort of bug in importing file to a XML document.

--- sd-store/store-ws_wsdl/src/main/java/store/ws/SDStoreImpl.java
+++ sd-store/store-ws_wsdl/src/main/java/store/ws/SDStoreImpl.java
@@ -3,6 +3,11 @@ package store.ws;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -14,6 +19,8 @@ import org.jdom2.Element;
 import org.jdom2.Document;
 import org.jdom2.output.XMLOutputter;
 import org.jdom2.output.Format;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.JDOMException;
 
 import pt.ulisboa.tecnico.sdis.store.ws.*;
 
@@ -27,15 +34,29 @@ public class SDStoreImpl implements SDStore {
 		
 		Document document = new Document(spreadsheet);
 		
-		XMLOutputter XMLoutput = new XMLOutputter();
+		XMLOutputter xmlOutput = new XMLOutputter();
 		//Print on console
+		/*
 		try {
-			XMLoutput.output(document, System.out);
+			xmlOutput.output(document, System.out);
 		} catch (IOException ex) {
 			System.out.println(ex.getMessage());
 		}
+		*/
 		
-		XMLoutput.setFormat(Format.getPrettyFormat());
+		//Create a buffered file writer
+		try {
+			FileWriter writer = new FileWriter("stored_files/"+docUser.getDocumentId()+".xml");
+			BufferedWriter bufferedWriter = new BufferedWriter(writer);
+			
+			//Output the XML document through the buffered file writer
+			xmlOutput.setFormat(Format.getPrettyFormat());
+			xmlOutput.output(document, bufferedWriter);
+			
+			bufferedWriter.close();
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
     public List<String> listDocs(String userId) throws UserDoesNotExist_Exception {
@@ -53,11 +74,50 @@ public class SDStoreImpl implements SDStore {
     	return null;
     }
     
-    private List<Element> getDocSet() {
-    	return null;
+    public List<Element> getDocSet() {
+    	List<Element> documents = new ArrayList<Element>();
+    	
+    	File[] files = new File("stored_files").listFiles();
+    	for (File file : files) {
+    		Element element = getElementFromFile(file);
+    		documents.add(element);
+    	}
+    	return documents;
     }
     
     private Element buildSpreadSheet () {
     	return null;
+    }
+    
+    private Element getElementFromFile (File file) {
+    	//Creates a XML builder using the SAXBuilder class
+    	SAXBuilder xmlBuilder = new SAXBuilder();
+    	
+    	try {
+    		try {
+    			//Generates a XML document through the XML builder using the imported document
+    			Document document = xmlBuilder.build(file);
+    			//Creates a new XML element equal to the root element from the document
+    			Element element = document.getRootElement();
+
+	    		return element;
+    		} catch (JDOMException ex) {
+    			System.out.println(ex.getMessage());
+    		}
+    	} catch (IOException ex) {
+    		System.out.println(ex.getMessage());
+    	}
+    	return null;
+    }
+    
+    public void destroyFile (String id) {
+    	File[] files = new File("stored_files").listFiles();
+    	for (File file : files) {
+    		Element element = getElementFromFile(file);
+    		if (element.getAttribute("id").getValue().equals(id)) {
+    			file.delete();
+    		}
+    		
+    	}
     }
 }
---
+++ sd-store/store-ws_wsdl/src/test/java/store/ws/GetDocuments.java
@@ -0,0 +1,43 @@
+package store.ws;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.jdom2.Element;
+
+import pt.ulisboa.tecnico.sdis.store.ws.DocAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.store.ws.DocUserPair;
+
+public class GetDocuments {
+	private static final String USERNAME = "username";
+	private static final String SHEET_ID = "2205";
+	private static DocUserPair pair = new DocUserPair();
+	private static SDStoreImpl store = new SDStoreImpl();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		pair.setUserId(USERNAME);
+		pair.setDocumentId(SHEET_ID);
+		
+		try {
+			store.createDoc(pair);
+		} catch (DocAlreadyExists_Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		store.destroyFile(SHEET_ID);
+	}
+	
+	@Test
+	public void printDocuments() {
+		for (Element element : store.getDocSet())
+			System.out.println(element);
+			//The errors so far are in the import of the file into XML
+	}
+}
