39ce509a92d15d953025b31ab8906fbcaea0fddd
Francisco Silveira <franciscosilveira463@gmail.com>
2015-04-13 20:41:27 +0100

    Re-implemented the file creation and content storing to work with byte arrays without conversion to XML elements. Implemented a success-expected test for each public functionality of SD-Store, including '@After' file deletion. A 'smart' byte Array declaration was used in the 'load' method where the Array size was equal to the file size. This closes #29, closes #30, closes #35, closes #36, closes #37, closes #38, closes #39, closes #40, closes #41, closes #42

--- sd-store/store-ws_wsdl/src/main/java/store/ws/StoreImpl.java
+++ sd-store/store-ws_wsdl/src/main/java/store/ws/StoreImpl.java
@@ -4,18 +4,18 @@ package store.ws;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.File;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import javax.jws.*;
+
 import store.ws.*; 
-//import javax.jws.WebMethod;
-//import javax.jws.WebParam;
-//import javax.jws.WebResult;
-//import javax.xml.ws.RequestWrapper;
-//import javax.xml.ws.ResponseWrapper;
 
 import org.jdom2.Element;
 import org.jdom2.Document;
@@ -25,6 +25,14 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.JDOMException;
 
 import pt.ulisboa.tecnico.sdis.store.ws.*;
+
+/*
+ * Interface methods:
+ * -createDoc
+ * -listDocs
+ * -store
+ * -load
+ */
 
 @WebService(
 	    endpointInterface="pt.ulisboa.tecnico.sdis.store.ws.SDStore", 
@@ -36,9 +44,34 @@ import pt.ulisboa.tecnico.sdis.store.ws.*;
 	)
 
 public class StoreImpl implements SDStore {
+	private static final String STORED = "stored_files";
+	private static final String TERMINATOR = ".txt";
+	private static final String SEPARATOR = Character.toString(File.separatorChar);
+	private static final int MAX_FILE_SIZE_BYTES = 1024; //10MB per file
 	 
+	/*
+	 *  Creates a new document in the provided user's repository.
+     *	In case this is the first operation on that user, 
+     *	a new repository is created for the new user.
+     *	Faults: a document already exists with the same id
+	 */
 	public void createDoc(DocUserPair docUser) throws DocAlreadyExists_Exception {
-		//CreateSpreadSheet();
+		String path = getFilePath(docUser);
+		
+		try {
+			File file = new File(path);
+			//If the user repository does not yet exist, create it
+			if (!file.getParentFile().exists()) {
+				file.getParentFile().mkdirs();
+			}
+			// Create the new file
+			file.createNewFile();
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	/*
 		Element spreadsheet = new Element("spreadsheet");
 		spreadsheet.setAttribute("id", docUser.getDocumentId());
 		spreadsheet.setAttribute("ownerUsername", docUser.getUserId());
@@ -53,7 +86,6 @@ public class StoreImpl implements SDStore {
 		} catch (IOException ex) {
 			System.out.println(ex.getMessage());
 		}
-		*/
 		
 		//Create a buffered file writer
 		try {
@@ -68,38 +100,168 @@ public class StoreImpl implements SDStore {
 		} catch (IOException ex) {
 			System.out.println(ex.getMessage());
 		}
-	}
-
+	*/
+	
+	/*
+	 * 	Lists the document ids of the user's repository.
+     *	Faults: user does not exist
+	 */
     public List<String> listDocs(String userId) throws UserDoesNotExist_Exception {
-    	return null;
+    	String path = getUserPath(userId);
+    	File[] files = new File(path).listFiles();
+    	List<String> fileList = new ArrayList<String>();
+    	
+    	for (File file : files) {
+    		fileList.add(file.getName());
+    	}
+    	
+    	return fileList;
     }
 
     public void store(DocUserPair docUserPair, byte[] contents)
     		throws CapacityExceeded_Exception, DocDoesNotExist_Exception, 
     		UserDoesNotExist_Exception
     {
+    	String path = getFilePath(docUserPair);
+    	File file = new File(path);
+    	FileOutputStream writer;
+    	BufferedOutputStream bufferedWriter = null;
     	
+    	try {
+    		writer = new FileOutputStream(file);
+    		bufferedWriter = new BufferedOutputStream(writer);
+    		
+    		//System.out.println(contents);
+    		bufferedWriter.write(contents);
+    		bufferedWriter.flush();
+    		bufferedWriter.close();
+    	} catch (IOException ex) {
+    		System.out.println("Failed IO");//ex.getMessage());
+    	} finally {
+    		try {
+    			if (bufferedWriter != null)
+    				bufferedWriter.close();	
+    		} catch (IOException ex) {
+    			System.out.println("Failed IO");//ex.getMessage());
+    		}
+    	}
     }
 
     public byte[] load(DocUserPair docUser) {
-    	return null;
-    }
-    
-    public List<Element> getDocSet() {
-    	List<Element> documents = new ArrayList<Element>();
+    	String path = getFilePath(docUser);
+    	File file = new File(path);
+    	float fileSize = file.length();
+    	//WARNING: casting float as int may not be safe
+    	byte[] content = new byte[(int)fileSize];
+    	FileInputStream reader;
+    	BufferedInputStream bufferedReader = null;
     	
-    	File[] files = new File("stored_files").listFiles();
-    	for (File file : files) {
-    		Element element = getElementFromFile(file);
-    		documents.add(element);
+    	try {
+    		reader = new FileInputStream(file);
+    		bufferedReader = new BufferedInputStream(reader);
+    		bufferedReader.read(content);
+    		bufferedReader.close();
+    	} catch (IOException ex) {
+    		System.out.println("Failed IO");
     	}
-    	return documents;
+    	
+    	return content;
     }
     
-    private Element buildSpreadSheet () {
-    	return null;
-    }
+    // ========== END OF INTERFACE METHODS ==========
     
+    /*
+	private Element getElementFromFile (File file) {
+		//Creates a XML builder using the SAXBuilder class
+		SAXBuilder xmlBuilder = new SAXBuilder();
+		
+		try {
+			try {
+				//Generates a XML document through the XML builder using the imported document
+				Document document = xmlBuilder.build(file);
+				//Creates a new XML element equal to the root element from the document
+				Element element = document.getRootElement();
+				System.out.println("Printing document:");
+				System.out.println(document);
+				System.out.println("Printing root element:");
+				System.out.println(element);
+				System.out.println("Printing element contents:");
+				System.out.println(element.getContent());
+				for (int i = 0; i<element.getContent().size(); i++) {
+					System.out.println(element.getContent().get(i).getValue());
+				}
+	
+	    		return element;
+			} catch (JDOMException ex) {
+				System.out.println(ex.getMessage());
+			}
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
+	*/
+	
+	public void destroyFile (String id) {
+		String path = STORED;
+		destroyFile(id, path);
+	}
+
+	/*
+	private Element getElementFromFile (File file) {
+		//Creates a XML builder using the SAXBuilder class
+		SAXBuilder xmlBuilder = new SAXBuilder();
+		
+		try {
+			try {
+				//Generates a XML document through the XML builder using the imported document
+				Document document = xmlBuilder.build(file);
+				//Creates a new XML element equal to the root element from the document
+				Element element = document.getRootElement();
+				System.out.println("Printing document:");
+				System.out.println(document);
+				System.out.println("Printing root element:");
+				System.out.println(element);
+				System.out.println("Printing element contents:");
+				System.out.println(element.getContent());
+				for (int i = 0; i<element.getContent().size(); i++) {
+					System.out.println(element.getContent().get(i).getValue());
+				}
+	
+	    		return element;
+			} catch (JDOMException ex) {
+				System.out.println(ex.getMessage());
+			}
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
+	*/
+	
+	private void destroyFile (String id, String path) {
+		File[] files = new File(path).listFiles();
+		for (File file : files) {
+			if (file.getName().equals(id+TERMINATOR)) {
+				file.delete();
+				break;
+			}
+			else if (file.isDirectory()) {
+				destroyFile(id, file.getPath());
+			}
+		}
+	}
+
+	private String getFilePath (DocUserPair docUser) {
+    	return (String) getUserPath(docUser.getUserId())
+				+ SEPARATOR + docUser.getDocumentId() + TERMINATOR;
+    }
+	
+	private String getUserPath (String username) {
+		return (String) STORED + SEPARATOR + username;
+	}
+    
+    /*
     private Element getElementFromFile (File file) {
     	//Creates a XML builder using the SAXBuilder class
     	SAXBuilder xmlBuilder = new SAXBuilder();
@@ -110,6 +272,15 @@ public class StoreImpl implements SDStore {
     			Document document = xmlBuilder.build(file);
     			//Creates a new XML element equal to the root element from the document
     			Element element = document.getRootElement();
+    			System.out.println("Printing document:");
+    			System.out.println(document);
+    			System.out.println("Printing root element:");
+    			System.out.println(element);
+    			System.out.println("Printing element contents:");
+    			System.out.println(element.getContent());
+    			for (int i = 0; i<element.getContent().size(); i++) {
+    				System.out.println(element.getContent().get(i).getValue());
+    			}
 
 	    		return element;
     		} catch (JDOMException ex) {
@@ -120,15 +291,5 @@ public class StoreImpl implements SDStore {
     	}
     	return null;
     }
-    
-    public void destroyFile (String id) {
-    	File[] files = new File("stored_files").listFiles();
-    	for (File file : files) {
-    		Element element = getElementFromFile(file);
-    		if (element.getAttribute("id").getValue().equals(id)) {
-    			file.delete();
-    		}
-    		
-    	}
-    }
+    */
 }
