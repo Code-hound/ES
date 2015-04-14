4091e58f0f4202e0ab5dcb13547b897b167b57b5
Francisco Silveira <franciscosilveira463@gmail.com>
2015-04-14 19:31:16 +0100

    Inserted all the code required to make the client connect to the UDDI

--- sd-store/store-ws-cli/src/main/java/store/cli/StoreClient.java
+++ sd-store/store-ws-cli/src/main/java/store/cli/StoreClient.java
@@ -1,24 +1,72 @@
 package store.cli;
 
 import java.util.List;
+import java.util.Map;
+import javax.xml.ws.*;
 
 import pt.ulisboa.tecnico.sdis.store.ws.*;
 
 public class StoreClient implements SDStore {
-	// ===== IMPLEMENTED METHODS =====
-	public void createDoc(DocUserPair) {
-		return null;
-	}
+	/** WS service */
+	private SDStore_Service storeService = null;
+	/** WS port (interface) */
+	private SDStore storeInterface = null;
+	/** WS endpoint address */
+    // default value is defined by WSDL
+    private String wsURL = null;
+    /** output option **/
+    private boolean verbose = false;
+    
+    public boolean isVerbose() {
+        return verbose;
+    }
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+    
+    public StoreClient() throws StoreClientException {
+    	createStub();
+    }
+    
+    public StoreClient(String wsURL) throws StoreClientException {
+    	this.wsURL = wsURL;
+        createStub();
+    }
+    
+    public void createStub() {
+    	if (verbose)
+            System.out.println("Creating stub ...");
+        storeService = new SDStore_Service();
+        storeInterface = storeService.getSDStoreImplPort();
+
+        if (wsURL != null) {
+            if (verbose)
+                System.out.println("Setting endpoint address ...");
+            BindingProvider bindingProvider = (BindingProvider) storeInterface;
+            Map<String, Object> requestContext = bindingProvider.getRequestContext();
+            requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, wsURL);
+        }
+    }
 	
-	public List<String> listDocs(String) {
-		return null;
+	public void createDoc(DocUserPair docUserPair)
+			throws DocAlreadyExists_Exception {
+		storeInterface.createDoc(docUserPair);
 	}
-	
-	public void store(DocUserPair, byte[]) {
-		return null;
+
+	public List<String> listDocs(String userId)
+			throws UserDoesNotExist_Exception {
+		return storeInterface.listDocs(userId);
 	}
-	
-	public byte[] load(DocUserPair) {
-		return null;
+
+	public void store(DocUserPair docUserPair, byte[] contents)
+			throws CapacityExceeded_Exception, DocDoesNotExist_Exception,
+			UserDoesNotExist_Exception {
+		storeInterface.store(docUserPair, contents);
 	}
+
+	public byte[] load(DocUserPair docUserPair)
+			throws DocDoesNotExist_Exception, UserDoesNotExist_Exception {
+		return storeInterface.load(docUserPair);
+	}
+
 }
---
+++ sd-store/store-ws-cli/src/main/java/store/cli/StoreClientException.java
@@ -0,0 +1,8 @@
+package store.cli;
+
+public class StoreClientException extends Exception {
+	
+	public StoreClientException(String message) {
+		super(message);
+	}
+}
