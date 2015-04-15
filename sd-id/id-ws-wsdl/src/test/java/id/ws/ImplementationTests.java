52797fda6db29359079ac77d9c199d805cebc84e
alinecaliente <alinecaliente@hotmail.de>
2015-04-15 22:52:08 +0100

    SD-ID test fixes

--- sd-id/id-ws-wsdl/src/test/java/id/ws/ImplementationTests.java
+++ sd-id/id-ws-wsdl/src/test/java/id/ws/ImplementationTests.java
@@ -14,52 +14,50 @@ import id.ws.IdImpl;
 
 public class ImplementationTests {
 	private static final String USERNAME = "username";
-	private static final String SHEET_ID = "2205";
-	private static DocUserPair pair = new DocUserPair();
+	private static final String USERNAME2 = "username2";
+	private static final String EMAIL = "a@b";
+	private static final String INVALID_USERNAME = "";
+	private static final String INVALID_EMAIL = "@b";
+	// private static final byte[] PASSWORD = "" /* COMO COMPARAR SE A PASS É GERADA AUTOMATICAMENTE? */
+	private static createUser user = new createUser();
+	private static createUser userDupEmail = new createUser(); //CONFIRMAR
+
 	private static IdImpl id = new IdImpl();
-	private static final String CONTENT = "This project is getting really hard";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		pair.setUserId(USERNAME);
-		pair.setDocumentId(SHEET_ID);
+		user.setUserId(USERNAME);
+		user.setEmailAddress(EMAIL);
 		
-		try {
-			id.createDoc(pair);
-		} catch (DocAlreadyExists_Exception ex) {
-			System.out.println(ex.getMessage());
-		}
+		userDupEmail.setUserId(USERNAME2);
+		userDupEmail.setEmailAddress(EMAIL);
 	}
 	
 	@Test
-	public void createUser() throws CapacityExceeded_Exception, DocDoesNotExist_Exception, 
-			UserDoesNotExist_Exception {
-		byte[] content_bytes = CONTENT.getBytes();
-		//System.out.println(content_bytes);
-		store.store(pair, content_bytes);
+	public void success() {
+		id.createUser(user);
+		
+		assertEquals("username", user.getUserId());
+		assertEquals("a@b", user.getEmailAddress());
 	}
 	
+	@Test(expected = EmailAlreadyExists_Exception.class)
+	public void testDuplicateEmail() {
+		id.createUser(userDupEmail);
+	}
+	
+	
 	@Test
-	public void listFiles() throws CapacityExceeded_Exception, DocDoesNotExist_Exception, 
-			UserDoesNotExist_Exception {
-		List<String> docs = store.listDocs(USERNAME);
+	public void renewPassword() {
+		/*List<String> docs = store.listDocs(USERNAME);
 		//System.out.println(docs);
 		assertTrue("File number does not match the expected",
-				(store.listDocs(USERNAME).size()==1));
-	}
-	
-	@Test
-	public void getFileContent() throws CapacityExceeded_Exception, DocDoesNotExist_Exception, 
-			UserDoesNotExist_Exception {
-		this.storeContent();
-		byte[] contentInBytes = store.load(pair);
-		String contentString = new String(contentInBytes);
-		assertTrue("Stored content does not match loaded content", 
-				(CONTENT.equals(contentString)));
+				(store.listDocs(USERNAME).size()==1));*/
 	}
 	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		id.destroyFile(SHEET_ID);
+		id.removeUser(USERNAME);
+		id.removeUser(USERNAME2);
 	}
 }
