bec53d5eec5a4e19b887fa1c1f35215e304bb059
JPZef <JPZef@users.noreply.github.com>
2015-05-12 21:44:49 +0100

    #79,  close #60 bynari function done

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignBinaryFunctionToCellService.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignBinaryFunctionToCellService.java
@@ -35,8 +35,11 @@ public class AssignBinaryFunctionToCellService extends BubbleDocsService {
 	private String auxBinaryFunction;
 	private String docname;
 	private String result;
-
-
+	private Literal arg1L;
+	private Literal arg2L;
+	private Reference arg1R;
+	private Reference arg2R;
+	
 	public AssignBinaryFunctionToCellService(String userToken, int docId, 
 			String cellId, String auxBinaryFunction) {
 		this.userToken = userToken;
@@ -47,7 +50,8 @@ public class AssignBinaryFunctionToCellService extends BubbleDocsService {
 
 	@Override
 	protected void dispatch(){// throws BubbleDocsException{
-
+		//FunctionArguments arg1;
+		//FunctionArguments arg2;
 		String username = resetUserLastAccess(userToken);
 		
 		if (username == null)
@@ -72,51 +76,127 @@ public class AssignBinaryFunctionToCellService extends BubbleDocsService {
 //auxOperator=2,3;1)
 		String auxOperator = auxBinaryFunction.substring(5);
 
-		//getting operator
-		String delims = "[,;)]+";
+		//retirar os agr's 1 e 2
+		String delims = "[,)]+";
 		String[] parseOpRef = auxOperator.split(delims);
-		int opNumber   = Integer.parseInt(parseOpRef[0]);
-		//cellReference = celula referenciada dentro da funcao binaria
-		int rowCellReference    = Integer.parseInt(parseOpRef[1]);
-		int columnCellReference = Integer.parseInt(parseOpRef[2]);
+		String auxArg1 = parseOpRef[0];
+		String auxArg2 = parseOpRef[1];
 				
 		//nao necessario so se verifica no calc value
-		/*int rowSpreadSheet    = sheet.getNumberRows();
+		int rowSpreadSheet    = sheet.getNumberRows();
 		int columnSpreadSheet = sheet.getNumberColumns();
 		// testa se a celula referenciada existe nas dimensoes da spreadsheet
-		if (!(rowCellReference    >= 0) ||
-			!(rowCellReference    <= rowSpreadSheet) ||
-			!(columnCellReference >= 0) ||
-			!(columnCellReference <= columnSpreadSheet))
-			throw new CellNotInSpreadSheetException(rowCellReference, columnCellReference, docId);
-		*/
-		Reference referenceAux = new Reference
-				(sheet, rowCellReference, columnCellReference);
-		Literal literalAux = new Literal(opNumber);
+		if (!(rowCell    >= 0) ||
+			!(rowCell    <= rowSpreadSheet) ||
+			!(columnCell >= 0) ||
+			!(columnCell <= columnSpreadSheet))
+			throw new CellNotInSpreadSheetException(rowCell, columnCell, docId);
+	
+		
+		//criar o literal ou referencia para o auxArg1
+		if (auxArg1.contains(";")){			
+			String delims1 = "[;]";
+			String[] parseOpRef1 = auxArg1.split(delims1);
+			int rowCellReference1 = Integer.parseInt(parseOpRef1[0]);
+			int columnCellReference1 = Integer.parseInt(parseOpRef1[1]);
+			this.arg1R = new Reference
+					(sheet, rowCellReference1, columnCellReference1);
+		}
+		else{
+			this.arg1L = new Literal(Integer.parseInt((auxArg1)));
+		}	
 		
 		
-		if (referenceAux.getCellReference().getProtect())
+		//criar o literal ou referencia para o arg2
+		if (auxArg2.contains(";")){			
+			String delims2 = "[;]";
+			String[] parseOpRef2 = auxArg2.split(delims2);
+			int rowCellReference2 = Integer.parseInt(parseOpRef2[0]);
+			int columnCellReference2 = Integer.parseInt(parseOpRef2[1]);
+			this.arg2R = new Reference
+					(sheet, rowCellReference2, columnCellReference2);	
+		}
+		else{
+			this.arg2L = new Literal(Integer.parseInt((auxArg2)));
+		}
+		
+		
+		//verifica se a cellId esta' protected para fazer assign
+		Reference referenceCellId = new Reference
+				(sheet, rowCell, columnCell);
+		if (referenceCellId.getCellReference().getProtect())
 			throw new ProtectedCellException(rowCell, columnCell);
-		
-
+	
 		switch (binaryFunction) {
 		case "ADD":
-			ADD binaryFuncA = new ADD(literalAux,referenceAux);
+			ADD binaryFuncA;
+			//verifica se o arg1 e' literal se falhar o if e' pq arg1 e' referencia
+			if (this.arg1L != null){
+				//verifica se o arg2 e' literal se falhar o if e' pq arg2 e' referencia
+				if (this.arg2L != null){
+					binaryFuncA = new ADD(this.arg1L,this.arg2L);}
+				else{
+					binaryFuncA = new ADD(this.arg1L,this.arg2R);}}
+			else{
+				if (this.arg2L != null){
+					binaryFuncA = new ADD(this.arg1R,this.arg2L);}
+				else{
+					binaryFuncA = new ADD(this.arg1R,this.arg2R);}
+			}
 			sheet.addContent(binaryFuncA, rowCell, columnCell);
 			this.result = ""+binaryFuncA.getContentValue();
 			return;
+			
 		case "SUB":
-			SUB binaryFuncS = new SUB(literalAux,referenceAux);
+			SUB binaryFuncS;
+			//EXPLICAÇÃO IGUAL AO DO ADD
+			if (this.arg1L != null){
+				if (this.arg2L != null){
+					binaryFuncS = new SUB(this.arg1L,this.arg2L);}
+				else{
+					binaryFuncS = new SUB(this.arg1L,this.arg2R);}}
+			else{
+				if (this.arg2L != null){
+					binaryFuncS = new SUB(this.arg1R,this.arg2L);}
+				else{
+					binaryFuncS = new SUB(this.arg1R,this.arg2R);}
+			}
 			sheet.addContent(binaryFuncS, rowCell, columnCell);
 			this.result = ""+binaryFuncS.getContentValue();
 			return;
+			
 		case "MUL":
-			MUL binaryFuncM = new MUL(literalAux,referenceAux);
+			MUL binaryFuncM;
+			//EXPLICAÇÃO IGUAL AO DO ADD
+			if (this.arg1L != null){
+				if (this.arg2L != null){
+					binaryFuncM = new MUL(this.arg1L,this.arg2L);}
+				else{
+					binaryFuncM = new MUL(this.arg1L,this.arg2R);}}
+			else{
+				if (this.arg2L != null){
+					binaryFuncM = new MUL(this.arg1R,this.arg2L);}
+				else{
+					binaryFuncM = new MUL(this.arg1R,this.arg2R);}
+			}
 			sheet.addContent(binaryFuncM, rowCell, columnCell);
 			this.result = ""+binaryFuncM.getContentValue();
 			return;
+			
 		case "DIV":
-			DIV binaryFuncD = new DIV(literalAux,referenceAux);
+			DIV binaryFuncD;
+			//EXPLICAÇÃO IGUAL AO DO ADD
+			if (this.arg1L != null){
+				if (this.arg2L != null){
+					binaryFuncD = new DIV(this.arg1L,this.arg2L);}
+				else{
+					binaryFuncD = new DIV(this.arg1L,this.arg2R);}}
+			else{
+				if (this.arg2L != null){
+					binaryFuncD = new DIV(this.arg1R,this.arg2L);}
+				else{
+					binaryFuncD = new DIV(this.arg1R,this.arg2R);}
+			}
 			sheet.addContent(binaryFuncD, rowCell, columnCell);
 			this.result = ""+binaryFuncD.getContentValue();
 			return; 
@@ -128,4 +208,5 @@ public class AssignBinaryFunctionToCellService extends BubbleDocsService {
 	public final String getResult() {
 		return result+"";
 	}
+	
 }
