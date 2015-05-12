ee1bd756fd26c6598d12bfce87452959e331871f
Francisco Silveira <franciscosilveira463@gmail.com>
2015-05-12 20:06:23 +0100

    Fixed wrong packages according to the new POM.xml

---
+++ sd-id/src/main/java/pt/ulisboa/tecnico/sdis/id/ws/impl/IdMain.java
@@ -0,0 +1,220 @@
+package pt.ulisboa.tecnico.sdis.id.ws.impl;
+
+import java.util.*;
+import java.security.Key;
+import pt.ulisboa.tecnico.sdis.id.ws.impl.*;
+
+import javax.xml.ws.Endpoint;
+import javax.naming.*;
+import javax.naming.directory.*;
+import javax.security.auth.login.*;
+import javax.security.auth.callback.*;
+import javax.security.auth.Subject;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+
+//provides helper methods to print byte[]
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+
+import ws.uddi.UDDINaming;
+
+@SuppressWarnings("unused")
+public class IdMain {
+
+    public static void main(String[] args) throws Exception {
+    	
+    	/*
+    	 * 
+    	 * With Minimal Implementation by Professor
+    	 * 
+    	 */
+		
+		String uddiURL = null;
+		String wsName = null;
+		String wsURL = null;
+		
+		IdImpl impl = null;
+        
+        Endpoint endpoint = null;
+        UDDINaming uddiNaming = null;
+
+        TestControl testImpl = null;
+    	
+    	final String plainText = args[0];
+        final byte[] plainBytes = plainText.getBytes();
+    	
+        // Check arguments and get plaintext
+		if (args.length == 0 || args.length == 2) {
+			System.err.println("Argument(s) missing!");
+			System.err.println("Usage: java " + IdMain.class.getName()
+					+ " wsURL OR uddiURL wsName wsURL");
+			System.err.println("args: (text)");
+			
+			return;
+		}
+		
+		if (args.length == 1) {
+			wsURL = args[0];
+			impl = new IdImpl(wsURL);
+		} else if (args.length >= 3) {
+			uddiURL = args[0];
+			wsName = args[1];
+			wsURL = args[2];
+			impl = new IdImpl(uddiURL, wsName, wsURL);
+		}
+        
+        try {
+        	
+        	impl.start();
+        	
+        	if ("true".equalsIgnoreCase(System.getProperty("ws.test"))) {
+        		IdImpl.reset();
+        		
+        		testImpl = new TestControl(wsURL + "/test");
+        		testImpl.start();
+        	}
+        	impl.awaitConnections();
+        } finally {
+            impl.stop();
+            
+            if (testImpl != null) {
+                testImpl.stop();
+            }
+        }
+        
+		try {
+        	
+            endpoint = Endpoint.create(new IdImpl(args));
+
+            // publish endpoint
+            System.out.printf("Starting %s%n", wsURL);
+            endpoint.publish(wsURL);
+            
+            // publish to UDDItype filter text
+            System.out.printf("Publishing '%s' to UDDI at %s%n", wsName, uddiURL);
+            uddiNaming = new UDDINaming(uddiURL);
+            uddiNaming.rebind(wsName, wsURL);            
+
+            // wait
+            System.out.println("Awaiting connections");
+            System.out.println("Press enter to shutdown");
+            System.in.read();
+
+        } catch(Exception e) {
+            System.out.printf("Caught exception: %s%n", e);
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (endpoint != null) {
+                    // stop endpoint
+                    endpoint.stop();
+                    System.out.printf("Stopped %s%n", wsURL);
+                }
+            } catch(Exception e) {
+                System.out.printf("Caught exception when stopping: %s%n", e);
+            }
+            try {
+                if (uddiNaming != null) {
+                    // delete from UDDI
+                    uddiNaming.unbind(wsName);
+					System.out.printf("Deleted '%s' from UDDI%n", wsName);
+                }
+            } catch(Exception e) {
+                System.out.printf("Caught exception when deleting: %s%n", e);
+            }
+        }
+    }  
+        /*
+         * 
+         * Demonstrate how to create an initial context to the SD-ID Server
+         * using Kerberos V5.
+         * 
+         * @usage: java
+         * @version: 1.0.0
+         * @author: Francisco Maria Calisto
+         * 
+         */
+		
+		
+        /*
+        // 1. Log in (to Kerberos)
+        LoginContext lc = null;
+        
+        try {
+        	lc = new LoginContext(IdMain.class.getName()); //TODO
+        	
+        	// Attempt authentication
+    	    // We might want to do this in a "for" loop to give
+    	    // user more than one chance to enter correct username/password
+    	    lc.login(); //TODO
+    	    
+        } catch (LoginException le) {
+    	    System.err.println("Authentication attempt failed" + le);
+    	    System.exit(-1);
+    	}
+        
+        // 2. Perform JNDI work as logged in subject
+    	//Subject.doAs(lc.getSubject(), new IdImpl(args));
+    
+	    */
+	    
+	    /*
+		 * 
+		 * This is a Java Web Service that supports asynchronous operations.
+	     *
+	     * The code is identical to other contract-first Web Services.
+	     *
+	     * The service is defined by the Java code with annotations
+	     * (code-first approach, also called bottom-up approach).
+	     *
+	     * The service runs in a standalone HTTP server.
+		 * 
+		 */
+	    
+	    /* ----- ATE AQUI TUDO BEM ----- */
+	/*	
+		 System.out.println("Text:");
+	     System.out.println(plainText);
+	     System.out.println("Bytes:");
+	     System.out.println(printHexBinary(plainBytes));
+	     
+
+	     // get a DES private key
+	     System.out.println("Generating DES key ...");
+	     KeyGenerator keyGen = KeyGenerator.getInstance("TripleDES");
+	     keyGen.init(168);
+	     Key key = keyGen.generateKey();
+	     System.out.println("Key:");
+	     System.out.println(printHexBinary(key.getEncoded()));
+
+	     // get a DES cipher object and print the provider
+	     Cipher cipher = Cipher.getInstance("TripleDES/ECB/PKCS5Padding");
+	     System.out.println(cipher.getProvider().getInfo());
+
+	     // encrypt using the key and the plaintext
+	     System.out.println("Text:");
+	     System.out.println(plainText);
+	     System.out.println("Bytes:");
+	     System.out.println(printHexBinary(plainBytes));
+
+	     System.out.println("Ciphering ...");
+	     cipher.init(Cipher.ENCRYPT_MODE, key);
+	     byte[] cipherBytes = cipher.doFinal(plainBytes);
+	     System.out.println("Result:");
+	     System.out.println(printHexBinary(cipherBytes));
+
+	     // decrypt the ciphertext using the same key
+	     System.out.println("Deciphering..." );
+	     cipher.init(Cipher.DECRYPT_MODE, key);
+	     byte[] newPlainBytes = cipher.doFinal(cipherBytes);
+	     System.out.println("Result:");
+	     System.out.println(printHexBinary(newPlainBytes));
+
+	     System.out.println("Text:");
+	     String newPlainText = new String(newPlainBytes); 
+	     System.out.println(newPlainText);
+		
+    }
+	*/
+}
---
+++ ws-handlers/src/main/java/ws/handler/HeaderHandler.java
@@ -0,0 +1,157 @@
+package ws.handler;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.Name;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.MessageContext.Scope;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+/**
+ *  This SOAPHandler shows how to set/get values from headers in
+ *  inbound/outbound SOAP messages.
+ *
+ *  A header is created in an outbound message and is read on an
+ *  inbound message.
+ *
+ *  The value that is read from the header
+ *  is placed in a SOAP message context property
+ *  that can be accessed by other handlers or by the application.
+ */
+public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
+
+	private static final String ID_PROPERTY = "ID";
+    private static final String TIME_PROPERTY = "Time";
+    private static final String HEADER_NAME = "Store-header";
+    private static final String ID_NAME = "clientID";
+    private static final String TIME_NAME = "messageTime";
+    private static final String NAMESPACE = "pt.tecnico.ulisboa.essd";
+    //private static String NAMESPACE_SERVER = "http://ws.store.wsdl";
+    
+    //
+    // Handler interface methods
+    //
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    public boolean handleMessage(SOAPMessageContext smc) {
+        //System.out.println("AddHeaderHandler: Handling message.");
+
+        Boolean outboundElement = (Boolean) smc
+                .get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+
+        try {
+            if (outboundElement.booleanValue()) {
+                System.out.println("Writing header in outbound SOAP message...");
+
+                // get SOAP envelope
+                String valueToInsert = (String) smc.get(ID_PROPERTY);
+                String messageTime = (String) smc.get(TIME_PROPERTY);
+                
+                SOAPMessage msg = smc.getMessage();
+                SOAPPart sp = msg.getSOAPPart();
+                SOAPEnvelope se = sp.getEnvelope();
+
+                // add header
+                SOAPHeader sh = se.getHeader();
+                if (sh == null)
+                    sh = se.addHeader();
+
+                // add header element (name, namespace prefix, namespace)
+                Name header = se.createName(HEADER_NAME, "d", NAMESPACE);
+                SOAPHeaderElement headerElement = sh.addHeaderElement(header);
+                
+                // add ID element with value
+                Name ID = se.createName(ID_NAME, "d", NAMESPACE);
+                SOAPElement IDElement = headerElement.addChildElement(ID);
+                IDElement.addTextNode(valueToInsert);
+                
+                // add time element with timestamp
+                Name time = se.createName(TIME_NAME, "d", NAMESPACE);
+                SOAPElement timeElement = headerElement.addChildElement(time);
+                timeElement.addTextNode(messageTime);
+                
+            } else {
+            	System.out.println("Reading header in inbound SOAP message...");
+
+                // get SOAP envelope header
+                SOAPMessage msg = smc.getMessage();
+                SOAPPart sp = msg.getSOAPPart();
+                SOAPEnvelope se = sp.getEnvelope();
+                SOAPHeader sh = se.getHeader();
+
+                // check header
+                if (sh == null) {
+                    System.out.println("Header not found. Also");
+                    return true;
+                }
+
+                // get first header element
+                Name name = se.createName(HEADER_NAME, "d", NAMESPACE);
+                Iterator<SOAPHeaderElement> headerIterator = sh.getChildElements(name);
+                
+                // check header element
+                if (!headerIterator.hasNext()) {
+                    System.out.println("Header element not found.");
+                    return true;
+                }
+                
+                SOAPElement header = (SOAPElement) headerIterator.next();
+                
+                Name ID = se.createName(ID_NAME, "d", NAMESPACE);
+                Iterator<SOAPElement> IDIterator = header.getChildElements(ID);
+                if (!IDIterator.hasNext()) {
+                	System.out.println("NO element of type "+ID_NAME);
+                	return true;
+                }
+                String IDValue = IDIterator.next().getValue();
+                smc.put(ID_PROPERTY, IDValue);
+                smc.setScope(ID_PROPERTY, Scope.APPLICATION);
+                
+                Name timestamp = se.createName(TIME_NAME, "d", NAMESPACE);
+                Iterator<SOAPElement> timeIterator = header.getChildElements(timestamp);
+                if (!timeIterator.hasNext()) {
+                	System.out.println("NO element of type "+TIME_NAME);
+                	return true;
+                }
+                String timeValue = timeIterator.next().getValue();
+                //System.out.println("[HANDLER] Timestamp: "+timeValue+"   Client ID: " + IDValue);
+                smc.put(TIME_PROPERTY, timeValue);
+                smc.setScope(TIME_PROPERTY, Scope.APPLICATION);
+                //System.out.println("Put everything on properties");
+            }
+        } catch (Exception e) {
+            System.out.print("Caught exception in handleMessage: ");
+            System.out.println(e);
+            System.out.println("Continue normal processing...");
+        }
+
+        return true;
+    }
+
+    public boolean handleFault(SOAPMessageContext smc) {
+        System.out.println("Ignoring fault message...");
+        return true;
+    }
+
+    public void close(MessageContext messageContext) {
+    }
+    
+    public static String getIDProperty() {
+    	return ID_PROPERTY;
+    }
+    
+    public static String getTimeProperty() {
+    	return TIME_PROPERTY;
+    }
+}
